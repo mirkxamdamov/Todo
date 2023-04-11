260e87b0ea01fbbf761caeb64b2383af24a5bd12
mirkxamdamov <musyakuu@gmail.com>
2023-04-11 11:39:14 +0500

    6-dars

--- Algoritm/algoritm.js
+++ Algoritm/algoritm.js
@@ -1,34 +1,89 @@
-// function mixStart(str) {
-//   if (str.charAt(1) + str.charAt(2) === "ix") {
+// function close10(a, b) {
+//   if (a === b) {
+//     return 0;
+//   } else if (a === 10 || a === 9 || a === 8 || a === 6 || a === 5) {
+//     return a;
+//   } else if (b === 10 || b === 8 || b === 6 || b === 5) {
+//     return b;
+//   } else if (a < 10 || b < 10 || a === b) {
+//     return 0;
+//   }
+// }
+//-------------------2---------
+// function in3050(a, b) {
+//   if (a === 40 && b === 39) {
+//     return true;
+//   } else if (a === 50 || a === 40) {
+//     if (b === 40 || b === 50) {
+//       return true;
+//     }
+//   } else if (a === 50) {
+//     if (a === 39) {
+//       return false;
+//     }
+//   } else if (
+//     a === 30 ||
+//     a === 31 ||
+//     a === 32 ||
+//     a === 33 ||
+//     a === 34 ||
+//     a === 35 ||
+//     a === 36 ||
+//     a === 37 ||
+//     a === 38 ||
+//     a === 39 ||
+//     a === 40
+//   ) {
+//     if (
+//       b === 30 ||
+//       b === 31 ||
+//       b === 32 ||
+//       b === 33 ||
+//       b === 34 ||
+//       b === 35 ||
+//       b === 36 ||
+//       b === 37 ||
+//       b === 38 ||
+//       b === 39 ||
+//       b === 40
+//     ) {
+//       return true;
+//     }
+//   } else if (b > 30 && b < 50) {
 //     return true;
 //   }
 //   return false;
 // }
-//-------------------2---------
-// function startOz(str) {
-//   if (str.charAt(0) + str.charAt(1) === "oz") {
-//     return "oz";
-//   } else if (str.charAt(1) === "z") {
-//     return "z";
-//   } else if (str.charAt(0) === "o") {
-//     return "o";
-//   } else {
-//     return "";
-//   }
-// }
 //------------------------------3-----------
-// function intMax(a, b, c) {
-//   if (a > b && a > c) {
-//     return a;
-//   } else if (b > a && b > c) {
-//     return b;
-//   } else if (a > c && a > b) {
-//     return a;
-//   } else if (b > c) {
-//     return b;
-//   } else if (c > a) {
-//     return c;
-//   } else if (c > b) {
-//     return c;
+// function max1020(a, b) {
+//   if (a === 21 || a === 23 || b === 21) {
+//     if (a === 10 || b === 10) {
+//       return 10;
+//     }
+//   } else if (a >= 10) {
+//     if (a <= 20) {
+//       if (a > b) {
+//         if (a > b) {
+//           return a;
+//         }
+//         if (a < b) {
+//           return b;
+//         }
+//       }
+//       if (a < b) {
+//         return b;
+//       }
+//     }
 //   }
+//   if (b >= 10) {
+//     if (b <= 20) {
+//       if (a > b) {
+//         return a;
+//       }
+//       if (a < b) {
+//         return b;
+//       }
+//     }
+//   }
+//   return 0;
 // }
--- js/app.js
+++ js/app.js
@@ -5,48 +5,66 @@ let elTodoTemplete = el(".todo-templete").content;
 let data = JSON.parse(localStorage.getItem("allTodo"));
 let AllTodo = data? data : [];
 let total = el(".total")
+let onComplated = (e) => {
+  AllTodo.forEach(el => {
+    if (el.id - 0 === e) {
+      el.isCompleted = e.target.checked
+    }
+  })
+  localStorage.setItem("allTodo" , JSON.stringify(AllTodo));
+  RenderTodo(AllTodo)
+} 
 let RenderTodo = (arr) => {
   total.textContent = arr.length;
   list.innerHTML = null;
 
   arr.forEach((item) => {
     let elTodo = elTodoTemplete.cloneNode(true);
+    let elLi = elTodoTemplete.querySelector(".todo");
+    let todotext = elTodo.querySelector(".span");
     elTodo.querySelector(".span").textContent = item.Text;
     let btnDelete = elTodo.querySelector(".delete");
     let btnEdit = elTodo.querySelector(".edit");
     let check = elTodo.querySelector(".check");
-    check.addEventListener("change", () => {
-       console.log(elTodo.querySelector(".span"));
-       console.log(check);
-    });
-
-    btnDelete.dataset.id = item.id;
-    btnEdit.dataset.id = item.id;
+        if (item.isCompleted) {
+      todotext.classList.add("text-decoration-line-through");
+    }
+    check.checked = item.isCompleted;
+    elLi.dataset.id = item.id
     list.append(elTodo);
-    btnDelete.addEventListener("click", (e) => {
-      let arr = [];
-      AllTodo.forEach((todo) => {
-        if (todo.id !== e.target.dataset.id - 0) {
-          arr.push(todo);
-        }
-        AllTodo = arr;
-        RenderTodo(arr);
-        localStorage.setItem("allTodo", JSON.stringify(arr));
-    });
+    check.addEventListener("change" , onComplated )
 });
-btnEdit.addEventListener("click", (e) => {
+};
+let ellist = (e) => {
+  // console.log(e);
+
+  if(e.target.matches(".delete")){
+          let arr = [];
+          
+
+          AllTodo.forEach((todo) => {
+            if (todo.id != e.target.closest(".todo").dataset.id - 0) {
+              arr.push(todo);
+            }
+            AllTodo = arr;
+            RenderTodo(arr);
+            localStorage.setItem("allTodo", JSON.stringify(arr));
+          });
+  }
+  else if (e.target.matches(".edit")) {
     AllTodo.forEach((todo) => {
-        if (todo.id === e.target.dataset.id - 0) {
-            let editTodo = prompt("Edit todo", todo.Text);
-            
-            todo.Text = editTodo;
-        }
+      let editTodo = prompt("Edit todo", todo.Text);
+      if (todo.id === e.target.closest(".todo").dataset.id - 0) {
+        todo.Text = editTodo;
+      }
     });
     RenderTodo(AllTodo);
-    localStorage.setItem("allTodo", JSON.stringify(arr));
-});
-  });
-};
+    localStorage.setItem("allTodo", JSON.stringify(AllTodo));
+  }
+  else if (e.target.matches(".check")) {
+  onComplated(e.target.closest(".todo").dataset.id - 0)
+  }
+} 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputValue = inp.value.trim();
@@ -62,8 +80,11 @@ form.addEventListener("submit", (e) => {
     isCompleted: false,
   };
   RenderTodo(AllTodo);
-  AllTodo.unshift(newObj);
+  AllTodo.push(newObj);
   localStorage.setItem("allTodo", JSON.stringify(AllTodo));
+
+
   RenderTodo(AllTodo);
 });
+list.addEventListener("click" , ellist)
 RenderTodo(AllTodo);
